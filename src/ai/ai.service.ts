09b89606dba3cd432345da81164553e541463703
ViMax <it.vimax@gmail.com>
2026-06-03 11:44:44 +0300

    update summary for AI

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -15,11 +15,49 @@ export class AiService {
   async improveSummary(summary: string): Promise<string> {
     const response = await this.ai.models.generateContent({
       model: 'gemini-2.5-flash',
-      contents: `Improve this resume summary:
+      contents: `Створи резюме на основі цього тексту:
       
-      ${summary}`,
+      ${summary}
+      
+      Відпоідь віддай в форматі json, ось такої структури:
+      {
+        "name": "",
+        "position": "",
+        "summary": "",
+        "template": "dark",
+        "skills": [],
+        "experience": [
+          {
+            "company": "",
+            "position": "",
+            "startDate": "",
+            "endDate": "",
+            "description": ""
+          },
+        ]
+      }
+        
+      Опис полів:
+      name - імя
+      position - на яку позицію претендує
+      summary - Опис про себе
+      template - тип шаблону, залиш template
+      experience - перелічення доствіду, де працював раніше. Поля:
+        company - назва команії де працював
+        position - ким працював
+        startDate - endDate - дата з якої по яку працював
+        description - опис, що робив`,
     });
 
-    return response.text ?? '';
+    return this.extractJson(response.text ?? '');
+  }
+
+  extractJson(text: string): string {
+    const cleaned = text
+      .replace(/```json/g, '')
+      .replace(/```/g, '')
+      .trim();
+
+    return JSON.parse(cleaned) as string;
   }
 }
