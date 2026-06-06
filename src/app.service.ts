dfb7fa7be2daf23fe01a01ebc79dbc7b1b575765
ViMax <it.vimax@gmail.com>
2026-06-06 11:41:00 +0300

    base config

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getGreetsYou(): string {
+    return '<a href="https://simple-cv.life">Simple CV greets you!</a>';
   }
 }
