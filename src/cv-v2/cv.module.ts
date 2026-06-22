8d90d4152810849a2edb88eba3cfccfb8fd950f2
ViMax <it.vimax@gmail.com>
2026-06-22 10:16:06 +0300

    added cqrs module

--- src/cv-v2/cv.module.ts
+++ src/cv-v2/cv.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AiDraftCvController } from '@draft/presentations/ai-draft-cv-controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CqrsModule],
   controllers: [AiDraftCvController],
 })
 export class CvModule {}
--- src/cv-v2/draft/presentations/ai-draft-cv-controller.ts
+++ src/cv-v2/draft/presentations/ai-draft-cv-controller.ts
@@ -2,6 +2,7 @@ import { GenerateAiDraftCommand } from '@draft/application/commands/generate-ai-
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { Authorization } from 'src/auth/decorators/authorization.decorator';
 import { Authorized } from 'src/auth/decorators/authorized.decorator';
+import { CommandBus } from '@nestjs/cqrs';
 
 @Controller('ai-draft-cv')
 export class AiDraftCvController {
